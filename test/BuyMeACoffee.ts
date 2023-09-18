2cdb2b883cea7aa340e1e96c44cef238fff5d797
Jayesh Shukla <hail_the_lord@Jayeshs-MacBook-Air.local>
2023-09-18 12:11:47 +0530

    - deployed the contract on sepolia network

---
+++ test/BuyMeACoffee.ts
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+import hre from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+
+// need to complete this test.
+
+describe("BuyMeACofee", function () {});
